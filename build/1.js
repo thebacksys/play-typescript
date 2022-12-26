b031a543136f6ce0553e0159ba83078b66a4a93b
ข้าวเหนียวไรเดอร์ <thebacksys@gmail.com>
2022-12-26 08:28:03 +0700

    add git ignore

--- build/1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Primitives
-// ชนิดข้อมูลพื้นฐาน => boolean number string
-// Explicit Type Declaration or infer
-// ประกาศตัวแปร แบบระบุ ชนิดข้อมูล
-let firstName = "first";
-// Implicit Type Declaration
-// ประกาศตัวแบบแบบ ไม่ระบุชนิดข้อมูล typescript จะตีความชนิดข้อมูล จากค่าที่ถูก assign
-let secondName = "second";
