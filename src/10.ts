03ca499a85f0b6c3916385f6a369c88afcbce861
ข้าวเหนียวไรเดอร์ <thebacksys@gmail.com>
2022-12-26 14:39:39 +0700

    example casting

--- src/10.ts
+++ src/10.ts
@@ -2,7 +2,8 @@
 
 let somePrice: unknown = "100";
 
-let discount: number = (20 / 100) * (somePrice as number);
+var discount: number = (20 / 100) * (somePrice as number);
+// var discount: number = (20 / 100) * somePrice;
 
 console.log("somePrice = ", somePrice);
 
